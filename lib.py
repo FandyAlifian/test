273054b6df175d575f634953a981d8e43cbe52cd
unknown <fandyalifian98@gmail.com>
2020-04-22 12:39:35 +0700

    memperbarui lib.py

--- lib.py
+++ lib.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-print_statistics(images, t_images, labels, t_labels):
+def print_statistics(images, t_images, labels, t_labels):
     #number of training dataset
     len(labels)
     #number of test dataset
@@ -14,3 +14,12 @@ print_statistics(images, t_images, labels, t_labels):
     for i in np.unique(t_labels):
         np.sum(t_labels==i)
         
+    def hypothesis(images, labels):
+        ekspo = []
+        for i in range(np.unique(labels)):
+            sumo = theta[0]
+            for j in range(len(images.iloc[0])):
+                sumo += np.sum(theta[i+1]*images.iloc[:,i])
+            ekspo.append(np.exp(sumo))
+         sumo = np.sum(ekspo)
+        return ekspo/sumo
